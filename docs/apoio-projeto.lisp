fb761d41ed388b4a916c84bc3a461f14c7c306b4
Patrick Battisti <1729patrick@gmail.com>
2019-11-21 08:44:44 +0000

    Material de apoio

---
+++ docs/apoio-projeto.lisp
@@ -0,0 +1,43 @@
+;;;; laboratorio7.lisp
+;;;; Disciplina de IA - 2019 / 2020
+;;;; Ficha de Laboratório nº7 - Apoio ao 1º projeto
+;;;; Autor: 
+
+
+;;; Tabuleiros
+
+
+(defun tabuleiro-teste ()
+"Tabuleiro de teste sem nenhuma jogada realizada"
+  '(
+    (94 25 54 89 21 8 36 14 41 96) 
+    (78 47 56 23 5 49 13 12 26 60) 
+    (0 27 17 83 34 93 74 52 45 80) 
+    (69 9 77 95 55 39 91 73 57 30) 
+    (24 15 22 86 1 11 68 79 76 72) 
+    (81 48 32 2 64 16 50 37 29 71) 
+    (99 51 6 18 53 28 7 63 10 88) 
+    (59 42 46 85 90 75 87 43 20 31) 
+    (3 61 58 44 65 82 19 4 35 62) 
+    (33 70 84 40 66 38 92 67 98 97)
+    )
+)
+
+(defun tabuleiro-jogado ()
+"Tabuleiro de teste igual ao anterior mas tendo sido colocado o cavalo na posição: i=0 e j=0"
+  '(
+    (T 25 54 89 21 8 36 14 41 96) 
+    (78 47 56 23 5 NIL 13 12 26 60) 
+    (0 27 17 83 34 93 74 52 45 80) 
+    (69 9 77 95 55 39 91 73 57 30) 
+    (24 15 22 86 1 11 68 79 76 72) 
+    (81 48 32 2 64 16 50 37 29 71) 
+    (99 51 6 18 53 28 7 63 10 88) 
+    (59 42 46 85 90 75 87 43 20 31) 
+    (3 61 58 44 65 82 19 4 35 62) 
+    (33 70 84 40 66 38 92 67 98 97)
+    )
+)
+
+
+;;; Exercicios
